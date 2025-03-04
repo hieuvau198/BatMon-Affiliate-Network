5143543cee7acd9ffbf169e7a3fe7def8658bc27
son1506 <169061311+son1506@users.noreply.github.com>
2025-03-04 15:57:49 +0700

    Update UI

--- src/app/routes/MainRoutes.jsx
+++ src/app/routes/MainRoutes.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom"; // Thêm Outlet để hiển thị nội dung con
 import AdminLayout from "../layouts/AdminLayout";
 import UserLayout from "../layouts/UserLayout/UserLayout";
 import Home from "../pages/Home";
@@ -8,6 +8,8 @@ import Dashboard from "../pages/Admin/Dashboard/Dashboard";
 import Employee from "../pages/Admin/Employee/Employee";
 import Booking from "../pages/Admin/Booking/Booking";
 import AboutUs from "../pages/About Us/AboutUs";
+import Login from "../components/Login/Login"; // Import Login
+import Register from "../components/Register/Register";
 
 const PageNotFound = lazy(() => import("../layouts/PageNotFound"));
 const ServerError = lazy(() => import("../layouts/ServerError/ServerError"));
@@ -17,18 +19,28 @@ export default function MainRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route element={<UserLayout />}>
+        {/* Route cho UserLayout */}
+        <Route
+          element={
+            <UserLayout>            
+              <Outlet /> 
+            </UserLayout>
+          }
+        >
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/register" element={<Register />} /> 
         </Route>
 
+        
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="employee" element={<Employee />} />
           <Route path="booking" element={<Booking />} />
         </Route>
 
+    
         <Route
           path="*"
           element={
